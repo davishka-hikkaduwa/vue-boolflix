4f31278c59b2237b9180d99ba1ca33f3e50d95cf
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-09-28 13:23:45 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '@/assets/styles/style.scss';
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
